c13d2c8d268fb7f0f81a6239022bf526deab8bbf
kidkrub <kritsadamoungkhao@gmail.com>
2024-04-25 20:27:59 +0700

    Implement get value from config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/kidkrub/assessment-tax/internal/pkg/config"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	cfg := config.New()
+	serverConfig := cfg.Server()
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	server := fmt.Sprintf("%s:%d", serverConfig.Hostname, serverConfig.PORT)
+	e.Logger.Fatal(e.Start(server))
 }
---
+++ internal/pkg/config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"os"
+	"strconv"
+)
+
+type env func(key string) string
+
+type cfg struct {
+	getEnv env
+}
+
+type Server struct {
+	Hostname string
+	PORT     int
+}
+
+type Database struct {
+	DatabaseUrl string
+}
+
+const (
+	cHostname    = "HOSTNAME"
+	cPort        = "PORT"
+	cDatabaseUrl = "DATABASE_URL"
+)
+
+func New() *cfg {
+	return &cfg{getEnv: os.Getenv}
+}
+
+func (c *cfg) Server() Server {
+	return Server{c.envString(cHostname, ""), c.envInt(cPort, 1323)}
+}
+
+func (c *cfg) Database() Database {
+	return Database{c.envString(cDatabaseUrl, "")}
+}
+
+func (c *cfg) envString(key, defaultValue string) string {
+	value := c.getEnv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
+
+func (c *cfg) envInt(key string, defaultValue int) int {
+	value := c.getEnv(key)
+
+	val, err := strconv.Atoi(value)
+	if err != nil {
+		return defaultValue
+	}
+	return val
+}
