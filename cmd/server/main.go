206f9e7878e5fb2d78286207a83a9493f3e0eeb9
kidkrub <kritsadamoungkhao@gmail.com>
2024-04-25 20:49:41 +0700

    Implement graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/kidkrub/assessment-tax/internal/pkg/config"
 	"github.com/labstack/echo/v4"
@@ -15,6 +19,24 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
 	})
+
 	server := fmt.Sprintf("%s:%d", serverConfig.Hostname, serverConfig.PORT)
-	e.Logger.Fatal(e.Start(server))
+
+	ctx, shutdown := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer shutdown()
+
+	go func() {
+		if err := e.Start(server); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+
 }
