d2eaffb4687f1e5f5028ec6647cf124617bc5c41
kidkrub <kritsadamoungkhao@gmail.com>
2024-04-26 12:30:07 +0700

    Init database and set default personal deduct value to database

---
+++ db/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS "deductions" (
+    id SERIAL PRIMARY KEY,
+    "name" TEXT UNIQUE,
+    maxAmount REAL
+);
+INSERT INTO "deductions" ("name", maxAmount)
+VALUES ('personal', 60000.0);
