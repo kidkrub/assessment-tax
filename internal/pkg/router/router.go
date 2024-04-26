13a3b289518ad46e0138d4f68bf84b090f5c6c4f
kidkrub <kritsadamoungkhao@gmail.com>
2024-04-26 15:36:58 +0700

    Implement using basic auth middleware for admin route

--- internal/pkg/router/router.go
+++ internal/pkg/router/router.go
@@ -6,7 +6,9 @@ import (
 
 	"github.com/kidkrub/assessment-tax/internal/pkg/handler/admin"
 	"github.com/kidkrub/assessment-tax/internal/pkg/handler/tax"
+	cmw "github.com/kidkrub/assessment-tax/internal/pkg/middleware"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func InitRoutes(db *sql.DB) *echo.Echo {
@@ -17,8 +19,11 @@ func InitRoutes(db *sql.DB) *echo.Echo {
 	th := tax.New()
 	ah := admin.New(db)
 
+	ag := e.Group("/admin")
+	ag.Use(middleware.BasicAuth(cmw.BasicAuthenticate()))
+
 	e.POST("/tax/calculations", th.TaxCalculateHandler)
-	e.POST("/admin/deductions/:type", ah.SetDeductionValueHandler)
+	ag.POST("/deductions/:type", ah.SetDeductionValueHandler)
 
 	return e
 }
